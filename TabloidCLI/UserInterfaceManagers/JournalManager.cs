ba3ec6613842469117a2981bbaf6ac04ac4e7af1
Alex Fun <alexanderthefun91@gmail.com>
2023-03-21 13:18:24 -0500

    initial commit with Journal Entries. Made the JournalRepository, Journal.cs and Journal Manager.

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, JournalRepository journalRepository, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Journal Entries");
+            Console.WriteLine(" 2) Add Journal Entry");
+            Console.WriteLine(" 3) Edit Journal Entry");
+            Console.WriteLine(" 4) Remove Journal Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Edit();
+                    return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+
+            }
+        }
+
+        private Journal Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please Choose a journal entry:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Journal> journals = _journalRepository.GetAll();
+
+            for (int i = 0; i < journals.Count; i++)
+            {
+                Journal journal = journals[i];
+                Console.WriteLine($" {i + 1} {journal.Title}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return journals[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine(journal);
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Journal Entry");
+            Journal journal = new Journal();
+
+            Console.Write("Title: ");
+            journal.Title = Console.ReadLine();
+
+            Console.Write("Text Content: ");
+            journal.TextContent = Console.ReadLine();
+
+            journal.CreationDate = DateTime.Now;
+        }
+
+        private void Edit()
+        {
+            Journal journalToEdit = Choose("Which entry would you like to edit?");
+            if (journalToEdit == null)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.Write("New Title (blank to leave unchanged): ");
+            string title = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                journalToEdit.Title = title;
+            }
+            Console.Write("New text content (blank to leave unchanged): ");
+            string textContent = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(textContent))
+            {
+                journalToEdit.TextContent = textContent;
+            }
+
+            _journalRepository.Update(journalToEdit);
+        }
+
+        private void Remove()
+        {
+            Journal journalToDelete = Choose("Which journal entry would you liek to remove?");
+            if (journalToDelete != null)
+            {
+                _journalRepository.Delete(journalToDelete.Id);
+            }
+        }
+    }
+}
