10c41a48a906845569407814b731f0760cebc6d4
Logan Gray <loganallengray@gmail.com>
2023-03-22 15:56:35 -0500

    posts can now have tags removed from them

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -230,17 +230,17 @@ namespace TabloidCLI.Repositories
             }
         }
 
-        public void DeleteTag(int authorId, int tagId)
+        public void DeleteTag(int postId, int tagId)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"DELETE FROM AuthorTAg 
-                                         WHERE AuthorId = @authorid AND 
+                    cmd.CommandText = @"DELETE FROM PostTag 
+                                         WHERE PostId = @postid AND 
                                                TagId = @tagId";
-                    cmd.Parameters.AddWithValue("@authorId", authorId);
+                    cmd.Parameters.AddWithValue("@postId", postId);
                     cmd.Parameters.AddWithValue("@tagId", tagId);
 
                     cmd.ExecuteNonQuery();
