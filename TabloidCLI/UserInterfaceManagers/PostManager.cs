02c597681077d9cc6687e97815b28b80a1303d70
Logan Gray <loganallengray@gmail.com>
2023-03-22 15:48:49 -0500

    posts can now have tags added to them

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -32,10 +32,11 @@ namespace TabloidCLI.UserInterfaceManagers
             
             Console.WriteLine("Post Menu");
             Console.WriteLine(" 1) List Posts");
-            Console.WriteLine(" 2) Add Post");
-            Console.WriteLine(" 3) Edit Post");
-            Console.WriteLine(" 4) Remove Post");
-            Console.WriteLine(" 5) Note Management");
+            Console.WriteLine(" 2) View Post Details");
+            Console.WriteLine(" 3) Add Post");
+            Console.WriteLine(" 4) Edit Post");
+            Console.WriteLine(" 5) Remove Post");
+            Console.WriteLine(" 6) Note Management");
             Console.WriteLine(" 0) Go Back");
 
             Console.Write("> ");
@@ -46,15 +47,25 @@ namespace TabloidCLI.UserInterfaceManagers
                     List();
                     return this;
                 case "2":
+                    Post post = Choose();
+                    if (post == null)
+                    {
+                        return this;
+                    }
+                    else
+                    {
+                        return new PostDetailManager(this, _connectionString, post.Id);
+                    }
+                case "3":
                     Add();
                     return this;
-                case "3":
+                case "4":
                     Edit();
                     return this;
-                case "4":
+                case "5":
                     Remove();
                     return this;
-                case "5":
+                case "6":
                     return new NoteManager(this, _connectionString);
                 case "0":
                     return _parentUI;
